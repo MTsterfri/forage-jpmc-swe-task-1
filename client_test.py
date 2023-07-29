7e4d81ad386585efc7dc694b1b42199613bf545f
Sterling Chargois <sterlingchargois@gmail.com>
2023-07-29 00:44:33 -0600

    Test Cases Updated

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,16 +8,28 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price']+quote['top_ask']['price'])/2))
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price']+quote['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculateRatioAEqualsZero(self):
+     self.assertEqual(getRatio(0, 5), 0)
+  def test_getRatio_calculateRatioBEqualsZero(self):
+     self.assertEqual(getRatio(5, 0), None)
+  def test_getRatio_calculateRatioALargerThanB(self):
+     self.assertEqual(getRatio(10, 2), 5)
+  def test_getRatio_calculateRatioBLargerThanA(self):
+     self.assertEqual(getRatio(2, 10), 0.2)
+  def test_getRatio_calculateRatioAEqualsB(self):
+     self.assertEqual(getRatio(1, 1), 1)
 
 
 
